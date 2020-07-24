43fbed54b85e0a2e767f6864c62455a259e946e9
itshimul <68614073+itshimul@users.noreply.github.com>
2020-07-24 15:00:50 +0600

    Add files via upload

---
+++ js/scrollTop.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 80) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+        return false;
+    });
+
+});
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
